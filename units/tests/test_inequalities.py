66b75522f87dc987c6175d65b3d81b136d3ec11c
Aran Donohue <aran@arandonohue.com>
2009-08-11 16:58:27 -0400

    Test plan for testing inequalities.

---
+++ units/tests/test_inequalities.py
@@ -0,0 +1,22 @@
+"""Test inequality comparisons between quantities and units, such as
+unit < unit
+unit <= unit
+unit > unit
+unit >= unit
+quantity < quantity
+quantity <= quantity
+quantity > quantity
+quantity >= quantity
+
+...
+each of these * {leaf units, the various composed units, named units}, 
+and each combination therein
+...
+
+each of these * valid and invalid comparisons
+
+...
+"""
+
+
+
