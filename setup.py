46d1cb33be411aa2afea82de7ccb7f47cf9aa11a
Aran Donohue <aran@arandonohue.com>
2009-08-17 00:38:54 -0400

    Prepare setup.py and string representation tests.
    --HG--
    rename : units/README => README

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import units
+
+setup(name='units',
+      version=units.__version__,
+      description="Python support for quantities with units",
+      long_description=units.__doc__,
+      author=units.__author__,
+      author_email=units.__contact__,
+      license=units.__license__,
+      url='http://www.arandonohue.com/hg/units',
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "Intended Audience :: System Administrators",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Scientific/Engineering",
+                   "Topic :: Software Development :: Libraries :: Python Module",
+                   "Topic :: Utilities"],
+      packages=['units', 'units.tests'],
+      platforms=["all"],
+      provides=['units'],
+      )
